33348fbd04bbce6381918bc91aabeeb9060c9444
abdelmattahmostafa <abdelfattahmostafa102@gmail.com>
2026-02-24 22:27:38 +0200

    Test

--- routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { register } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/register", register);
-
-export default router;
