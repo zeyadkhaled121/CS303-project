08d8385be569075bd9a707b9cbf7b78bcd992932
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-02-25 00:26:59 +0200

    Refactor API routes and database connection

--- server/app.js
+++ server/app.js
@@ -2,25 +2,24 @@ import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { connectDB } from "./database/db.js"; 
 import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
 import authRouter from "./routes/authRouter.js";
 
 export const app = express();
+
 config({ path: "./config/config.env" });
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    credentials: true, 
 }));
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/auth", authRouter);
 
-connectDB();
+app.use("/api/v1/user", authRouter);
 
 app.use(errorMiddleware);
