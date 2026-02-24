2ec4fd16293410a6c7febc6492f963d993dd69dc
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-02-24 22:08:47 +0200

    Fix status code variable naming in error middleware

--- server/middleware/errorMiddlewares.js
+++ server/middleware/errorMiddlewares.js
@@ -4,39 +4,47 @@ class Errorhandler extends Error {
     this.statusCode = statusCode;
   }
 }
+
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal server error";
   err.statusCode = err.statusCode || 500;
+
   console.log(err);
+
   if (err.code === 11000) {
-    const statuscode = 400;
+    const statusCode = 400; 
     const message = `Duplicate Field Value Entered`;
-    err = new Errorhandler(message, statuscode);
+    err = new Errorhandler(message, statusCode);
   }
 
   if (err.name === "JsonWebTokenError") {
-    const statuscode = 400;
-    const message = `Json Web Token is invalid,Try again.`;
-    err = new Errorhandler(message, statuscode);
+    const statusCode = 400;
+    const message = `Json Web Token is invalid, Try again.`;
+    err = new Errorhandler(message, statusCode);
   }
+
   if (err.name === "TokenExpiredError") {
-    const statuscode = 400;
-    const message = `Json Web Token is invalid,Try again.`;
-    err = new Errorhandler(message, statuscode);
+    const statusCode = 400;
+    const message = `Json Web Token is expired, Try again.`;
+    err = new Errorhandler(message, statusCode);
   }
+
   if (err.name === "CastError") {
-    const statuscode = 400;
-    const message = `Resource not found . Invalid:${err.path}`;
-    err = new Errorhandler(message, statuscode);
+    const statusCode = 400;
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new Errorhandler(message, statusCode);
   }
+
   const errorMessage = err.errors
     ? Object.values(err.errors)
         .map((error) => error.message)
         .join(" ")
     : err.message;
-    return res.status(err.statuscode).json({
-        success:false,
-        message:errorMessage,
-    })
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: errorMessage,
+  });
 };
+
 export default Errorhandler;
