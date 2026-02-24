3c2495ff3b46f3a67b9414d2252493c6b9042dec
Mostaf3Ma7moudALI <mzaytoon233@gmail.com>
2026-02-24 04:24:09 +0200

    first commit

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {useSelector} from "react-redux"; 
 
 const Home = () => {
+  const [isSideBarOpen,SetIsSideBarOpen] = useState(false);
+  const [selectedComponent,setSelectedComponent] = useState(false);
+
+  const {} = useSelector();
+  
+  
   return <></>;
 };
 
