b11695fab6bfe8c7003f3cd79459ba26f768d05d
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2026-02-24 21:02:55 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,20 +1,25 @@
 import express from "express";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import { connectDB } from "./database/db.js";
-import{errorMiddleware}from"./middleware/errorMiddlewares.js"
-export const app =express();
-config({path:"./config/config.env"});
-app.use(cors({
-    origin : [process.env.FRONTEND_URL],
-    methods : ["GET","POST","PUT","DELETE"],
-    credentials : true,
+import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
+import authRouter from "./routes/authRouter.js"
 
+export const app = express();
+config({path: "./config/config.env"});
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET" , "POST" , "PUT" , "DELETE"],
+    credentials: true,
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-console.log("App.js is correctly imported and running...");
+app.use("/api/v1/auth" , authRouter);
+
+
 connectDB();
+
 app.use(errorMiddleware);
