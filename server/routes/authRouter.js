40f23470ebb3b49437c02c5e5d6ff90f626ad43f
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-02-25 00:25:23 +0200

    Add authentication routes for user actions

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { 
+    register, 
+    verifyEmail, 
+    loginUser, 
+    logoutUser, 
+    forgotPassword, 
+    resetPassword ,
+    updatePassword,
+    getUserProfile
+} from "../controllers/authController.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js"; 
 
 const router = express.Router();
 
 router.post("/register", register);
-
+router.post("/verify-email", verifyEmail);
+router.post("/login", loginUser);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset", resetPassword);
+router.put("/password/update", isAuthenticatedUser, updatePassword);
+router.get("/logout", isAuthenticatedUser, logoutUser);
+router.get("/me", isAuthenticatedUser, getUserProfile);
 export default router;
