1e3aa8cca79552363f80659e6e65814edaa2d845
Mostafa-1 <mzaytoon233@gmail.com>
2026-03-03 20:59:27 +0200

    MOb 2

--- mobile/reactApp/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ImageBackground } from "expo-image"
-import { Button, Image, Text, View, ScrollView } from "react-native"
-
-export default function App() {
-  return (
-    <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
-    </ScrollView>
-  )
-}
