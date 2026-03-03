b06be30e91be20e7928719e0b96e37062bc69d02
Mostafa-1 <mzaytoon233@gmail.com>
2026-03-03 19:08:55 +0200

    Mobile commit

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-// pages/index.js - Export all pages
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
-export { default as HomePage } from './HomePage';
