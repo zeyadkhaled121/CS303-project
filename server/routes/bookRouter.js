e33522e043996801ef694747a32f29455e9dda46
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2026-03-03 23:05:18 +0200

    Add files via upload

---
+++ server/routes/bookRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createBook, updateBook, deleteBook } from "../controllers/bookController.js";
+import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+// كل المسارات دي محمية ومفيش حد يقدر يدخلها غير الـ Admin
+router.post("/add", isAuthenticatedUser, authorizeRoles("Admin"), createBook);
+router.put("/update/:id", isAuthenticatedUser, authorizeRoles("Admin"), updateBook);
+router.delete("/delete/:id", isAuthenticatedUser, authorizeRoles("Admin"), deleteBook);
+
+export default router;
