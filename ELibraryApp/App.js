9e8f1f26b6a8db00361af6e9c340e44fbeda62dd
Mostafa-1 <mzaytoon233@gmail.com>
2026-03-03 22:23:42 +0200

    LOgin page in mob

--- ELibraryApp/App.js
+++ ELibraryApp/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
-  return <AppNavigator />;
+  return (<><AppNavigator /><LoginScreen /></>
+  );
 }
