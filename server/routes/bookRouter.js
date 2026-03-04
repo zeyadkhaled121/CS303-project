492ce6ac9a6b08c354a14680f55c3db7d23c715e
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-03-04 22:42:52 +0200

    Add getAllBooks route and update role authorization
    Added a route to get all books for authenticated users and updated role authorization for book management.

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { createBook, updateBook, deleteBook } from "../controllers/bookController.js";
+import { getAllBooks, createBook, updateBook, deleteBook } from "../controllers/bookController.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/add", isAuthenticatedUser, authorizeRoles("Admin"), createBook);
-router.put("/update/:id", isAuthenticatedUser, authorizeRoles("Admin"), updateBook);
-router.delete("/delete/:id", isAuthenticatedUser, authorizeRoles("Admin"), deleteBook);
+// Any authenticated user can view books
+router.get("/getall", isAuthenticatedUser, getAllBooks);
 
+//  Admin and Super Admin can manage books
+router.post("/add", isAuthenticatedUser, authorizeRoles("Admin", "Super Admin"), createBook);
+router.put("/update/:id", isAuthenticatedUser, authorizeRoles("Admin", "Super Admin"), updateBook);
+router.delete("/delete/:id", isAuthenticatedUser, authorizeRoles("Admin", "Super Admin"), deleteBook);
 
 export default router;
