612fb6a7033307603923d99204fa4d94836ac386
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-03-04 02:19:30 +0200

    Refactor sendToken to exclude cookie and add error field
    Removed cookie handling and added error field in response.

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,30 +1,23 @@
 import jwt from "jsonwebtoken";
 
+
 export const sendToken = (user, statusCode, res, message) => {
-    
     const secretKey = process.env.JWT_SECRET || "fallback_secret_key_12345";
-    const cookieExpire = Number(process.env.COOKIE_EXPIRE) || 7; 
     const jwtExpire = process.env.JWT_EXPIRES || "7d";
 
     const token = jwt.sign({ id: user.id }, secretKey, {
         expiresIn: jwtExpire,
     });
 
-    const options = {
-        expires: new Date(
-            Date.now() + cookieExpire * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-        secure: false, 
-        sameSite: "Lax", 
-    };
+    const { password, verificationCode, verificationCodeExpire, resetPasswordToken, resetPasswordExpire, ...userData } = user;
 
-    const { password, ...userData } = user;
-
-    res.status(statusCode).cookie("token", token, options).json({
+    res.status(statusCode).json({
         success: true,
         message,
-        user: userData,
-        token,
+        data: {
+            user: userData,
+            token,
+        },
+        error: null,
     });
 };
